037ea8c2b174cf28aaf5652c821e9a6baa15a22c
Mar Ndiaye <mar.ndiaye221@gmail.com>
2023-02-26 14:59:59 +0000

    Ajout d'un nouveau message de bienvenue Fusion des deux modifications éffectués sur le fichier mains.c

--- main.c
+++ main.c
@@ -4,6 +4,10 @@
 int main(void) {
 	
 	printf("Bienvenue sur git");
+<<<<<<< HEAD
 scandf("%git");
+=======
+	printf("Git est un outil permettant de gérer les différentes versions d'un projet");
+>>>>>>> 44ceac9 (Ajout d'un nouveau message de bienvenue)
 	return 0;
 }
