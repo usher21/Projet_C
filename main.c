c95e47777f590e36f84098178e9bae6615472236
Mar Ndiaye <mar.ndiaye221@gmail.com>
2023-02-26 21:12:51 +0000

    Résolution du conflit

--- main.c
+++ main.c
@@ -2,12 +2,10 @@
 #include <stdio.h>
 
 int main(void) {
-	
+
 	printf("Bienvenue sur git");
-<<<<<<< HEAD
-scandf("%git");
-=======
+	scandf("%git");
 	printf("Git est un outil permettant de gérer les différentes versions d'un projet");
->>>>>>> 44ceac9 (Ajout d'un nouveau message de bienvenue)
+
 	return 0;
 }
